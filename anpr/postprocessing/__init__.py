5889d4817fbe27f8eb92ed6eae48cc80f0fc14df
quick-1y <l0nglife3025@gmail.com>
2025-12-10 15:07:05 +0300

    Delete anpr directory

--- anpr/postprocessing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Постпроцессинг и валидация автомобильных номеров."""
-
-from .postprocessor import PlatePostProcessor, PlateValidationResult
-from .registry import list_country_profiles
-
-__all__ = ["PlatePostProcessor", "PlateValidationResult", "list_country_profiles"]
