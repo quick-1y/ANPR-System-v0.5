a9e2eb37e1f6c2f204b1074f8ec19843183fbaf3
quick-1y <l0nglife3025@gmail.com>
2025-12-11 10:44:06 +0300

    Delete anpr directory

--- anpr/postprocessing/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Постобработка распознанных номеров."""
-
-from .plate_validator import PlatePostProcessor, PlateValidator, ValidationResult
-
-__all__ = ["PlatePostProcessor", "PlateValidator", "ValidationResult"]
